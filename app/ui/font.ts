85b1088674ecb4ccee02d22bc7d4b96b2da1c10b
Talha-24 <talhabhatti82542@gmail.com>
2025-08-12 04:33:10 +0500

    Updated Code

---
+++ app/ui/font.ts
@@ -0,0 +1,3 @@
+import {Inter,Lusitana} from "next/font/google"
+export const inter=Inter({subsets:["latin"],weight: ["500","800"]})
+export const lusitana=Lusitana({subsets: ["latin"],weight: "700"});
